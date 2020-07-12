9b68afa73a65511b1bfe71ab7b51d7009e9e3f79
noonesthere <d.zlobenets@gmail.com>
2020-07-12 22:36:40 +0300

    wth episode

--- episode_2_additional/main.js
+++ episode_2_additional/main.js
@@ -1,30 +1,36 @@
+    let TIMES = 520,
+    ANGLE =  Math.PI * 2,
+    WEIGHT = 5,
+    currentAngle = 0;
+ 
+
 window.onload = function(){
     var canvas = document.getElementById("canvas"),
     context = canvas.getContext("2d"),
     width = canvas.width = window.innerWidth,
     height = canvas.height = window.innerHeight,
-    TIMES = 20,
-    ANGLE =  Math.PI * 2,
-    WEIGHT = 5,
     centerX = width / 2,
     centerY = height / 2;
+    
+    let update = test(context, centerX, centerY);
 
-  // context.fillRect(0, 0, width, height);
-//     context.translate(0, height / 2);
-  //   context.scale(1, -1);
-   
-   
-
-   for(var angle = 0; angle < ANGLE; angle += 0.5){
+    setInterval(update, 1);
+         
+   }
+function test(context, centerX, centerY){
+ let angle  = 0;
+ return function(){
+      angle += .5;
+//      angle = angle > ANGLE ? 0 : angle;
         var x =  Math.cos(angle) ,
             y = Math.sin(angle) ;
-//       context.fillRect(x, y, WEIGHT, WEIGHT);  
+
          context.beginPath();
          context.moveTo(centerX, centerY);
          context.lineTo(x * TIMES + centerX,  y * TIMES + centerY);
          context.stroke();
-         
-   }
-
+ 
+    } 
 
 }
+
